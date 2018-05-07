fe28a42c156861057252adfaacd68988ea217c18
justPeterM8 <piotrg14@gmail.com>
2018-05-07 15:22:08 +0200

    Project setup

---
+++ src/main/java/students/polsl/eatnearbackend/EatnearBackendApplication.java
@@ -0,0 +1,12 @@
+package students.polsl.eatnearbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatnearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatnearBackendApplication.class, args);
+	}
+}
